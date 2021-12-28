0583bd6cdb1b1f5ee655b32cc557b95af949bc07
shan-star <68067000+shan-star@users.noreply.github.com>
2021-12-28 15:23:38 +0800

    Update actions.js

--- src/store/actions.js
+++ src/store/actions.js
@@ -21,22 +21,5 @@ export const actions = {
       }
     })
   }
-  // addToCart(state, payload) {
-  //   // console.log(payload);
-  //   //判断当前仓库是否已有该商品
-  //   //最普通的for-i 循环,好点的of循环，indexof是在匹配字符串
-  //   //高级find 函数
-  //   let oldGood = null;
-  //   oldGood = state.cartGoods.find(item => item.iid === payload.iid);
-  //   if(oldGood){
-  //     //找得到，即老商品了
-  //     oldGood.count++;
-  //     console.log('商品数量+1');
-  //   }else{
-  //     //新商品
-  //     payload.count = 1;
-  //     state.cartGoods.push(payload);
-  //     console.log('添加商品');
-  //   }
-  // }
+
 }
